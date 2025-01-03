322671e9a9d6b2c97220ca2826cb8c2f8cab8d0e
marcos <dev.marcosanjos@gmail.com>
2025-01-03 09:43:02 -0300

    feat: Integrate Input and Select components into NewContact page for enhanced form functionality

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
+import Input from "../../components/input";
+import Select from "../../components/select";
 
 export default function NewContact() {
-    return <PageHeader title="Novo contato" />;
+    return (
+        <>
+            <PageHeader title="Novo contato" />
+            <Input type="text" placeholder="Nome" />
+            <Select>
+                <option value="123">Instagram</option>
+                <option value="123">Instagram</option>
+                <option value="123">Instagram</option>
+                <option value="123">Instagram</option>
+            </Select>
+        </>
+    );
 }
